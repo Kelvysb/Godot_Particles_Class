4a4fb9e2b31695cb6fd5815fbd3a6d200cd77549
KelvysB <contato@kelvysb.com>
2023-08-18 21:49:24 -0300

    Progress

--- Project/Asteroid.gd
+++ Project/Asteroid.gd
@@ -1,15 +1,14 @@
 extends RigidBody3D
 class_name Asteroid
+var randScale = Vector3(1,1,1)
+var randSpeed = 20.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	var randScale = randf_range(0.5, 2.5)
-	scale.x = randScale
-	scale.y = randScale
-	scale.z = randScale
-	var randSpeed = randf_range(20.0, 40.0)
+	randScale = Vector3(randf_range(0.5, 2.5),randf_range(0.5, 2.5),randf_range(0.5, 2.5))
+	scale = randScale
+	randSpeed = randf_range(20.0, 40.0)
 	apply_impulse(Vector3(0,0,randSpeed))
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func  _physics_process(delta):
+	scale = randScale
